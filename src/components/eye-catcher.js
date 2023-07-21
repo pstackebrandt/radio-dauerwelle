29ec3f457aaa71603a1ca06838c66d3a16be8e62
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-21 16:44:45 +0200

    Eye catcher: Use all eye catcher data, show first at start, show next after 10 s, restart with last uses useState and useEffect

--- src/components/eye-catcher.js
+++ src/components/eye-catcher.js
@@ -1,6 +1,6 @@
 // file: eye-catcher.js
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./eye-catcher.css";
 
 function NextEyeCatcher({ eyeCatcher }) {
@@ -9,19 +9,35 @@ function NextEyeCatcher({ eyeCatcher }) {
       <img src={eyeCatcher.imgSource} alt={`Foto von ${eyeCatcher.photoCredit}`} width="100px" height="100px" />
 
       <h2>{eyeCatcher.header}</h2>
-      <p>{eyeCatcher.teaser}<a href="/">weiterlesen</a></p>
+      <p>
+        {eyeCatcher.teaser}
+        <a href="/">weiterlesen</a>
+      </p>
     </div>
   );
 }
 
+/* Show 1 eye catcher on screen. If we get data for more than 1 rerun eyecatcher after a while.  */
 function EyeCatcher({ eyeCatchers }) {
   // start with first eyeCatcher data object.
-  // after 10 seconds switch to next eyeCatcher data object.
+  // after n seconds switch to next eyeCatcher data object.
   // do so till every eyeCatcher data object has been shown.
   // then start again with first eyeCatcher data object.
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // Funktion zum Wechseln des Indexes in einem Intervall von 10 Sekunden
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % eyeCatchers.length);
+    }, 10000);
+
+    // Aufräumarbeiten beim Entfernen der Komponente
+    return () => clearInterval(interval);
+  }, [eyeCatchers.length]);
+
   return (
     <div className="eye-catchers">
-      {<NextEyeCatcher eyeCatcher={eyeCatchers[0]} />}
+      <NextEyeCatcher eyeCatcher={eyeCatchers[currentIndex]} />
     </div>
   );
 }
