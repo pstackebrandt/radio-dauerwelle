96beacfff77c4a25797dffe6b6a7a742fc08ad48
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-25 14:21:11 +0200

    top-nav component: styled with bootstrap (not react bootstrap)

--- src/pages/top-nav.js
+++ src/pages/top-nav.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function TopNav() {
     return (
         <div className='top-nav'>
-            <nav>
-                <NavLink to="/">Willkommen</NavLink><br />
-                <NavLink to="playlist">Playlist</NavLink><br />
-                <NavLink to="advertisement">Werbung buchen</NavLink><br />
-                <NavLink to="employees">Mitarbeiter</NavLink>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <NavLink className="nav-link" activeClassName="active" exact to="/">Willkommen</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/playlist">Playlist</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/advertisement">Werbung buchen</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/employees">Mitarbeiter</NavLink>
+                    </div>
+                </div>
             </nav>
         </div>
     );
