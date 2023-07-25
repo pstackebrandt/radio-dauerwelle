ee707e1ec2134713ded76a7070795cc0e12ef35f
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-25 19:47:04 +0200

    eye catchers: hold eye catchers data additionally in a separate component (and file).

---
+++ src/data/eye-catchers-data.js
@@ -0,0 +1,48 @@
+// EyeCatchersData.js
+import React, { useState, useEffect } from 'react';
+import EyeCatcher from '../pages/eye-catcher';
+
+export default function EyeCatchersData() {
+    const [eyeCatchers, setEyeCatchers] = useState([]);
+
+    useEffect(() => {
+        // angenommen, dies ist Ihre Funktion zum Abrufen von Daten vom Server
+        async function fetchData() {
+            const initialData = [
+                {
+                  id: 0,
+                  imgSource: "/images/pexels-eric-esma-894156.jpg",
+                  header: "Samy Deluxe im Anmarsch",
+                  teaser: "Samy Delux hat eine neue Single veröffentlicht. Simon Desu produzierte die Beats",
+                  photoCredit: "Eric Esma"
+                },
+                {
+                  id: 1,
+                  imgSource: "/images/pexels-anete-lusina-5239964.jpg",
+                  header: "2Pac - Lebt",
+                  teaser: "Immer wieder gibt es Gerüchte das 2 Pac noch lebt. Neuste Sichtung in Texas...",
+                  photoCredit: "Anete Lusina"
+                },
+                {
+                  id: 2,
+                  imgSource: "/images/pexels-maria-pop-339805.jpg",
+                  header: "Eddie hat ausgedient!",
+                  teaser: "Eddie bekommt ein neues Design, Zombies sind nicht meher zeitgemäß!",
+                  photoCredit: "Maria Pop"
+                },
+            ];
+
+            //setzen Sie den initialen Daten
+            setEyeCatchers(initialData);
+
+            // hier würden Sie Ihren Service oder Server aufrufen, um die Daten zu aktualisieren
+            // z.B.: const response = await fetch('your-server-url');
+            // const data = await response.json();
+            // setEyeCatchers(data);
+        }
+
+        fetchData();
+    }, []); // [] bedeutet, dass dieser useEffect nur einmal ausgeführt wird, wenn die Komponente gemountet wird
+
+    return <EyeCatcher eyeCatchers={eyeCatchers}/>;
+}
