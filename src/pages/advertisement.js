076490b1bf7db33b0819b0020adacaf79d45c979
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-27 20:22:53 +0200

    Advertisement: Suppress misleading warning

--- src/pages/advertisement.js
+++ src/pages/advertisement.js
@@ -56,6 +56,7 @@ export default function Advertisement() {
         if (hadToCalculatePriceOnce) {
             setTotalCost(calculateTotalCost());
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [spotLengthSeconds, placementBeforeNews, placementAfterNews, placementTraffic, hadToCalculatePriceOnce, frequency]);
 
 
